25344a81cc6cc25efed21f225e4b2bf90acc8ec0
mehrdadkhandan1 <1mehrdadkhandan1@gmail.com>
2024-05-04 01:34:02 +0330

    Revert "search bar"
    This reverts commit a6064a47e4611c730375e60e288307f200d39d31.

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,10 @@
 import React from 'react'
-import Layout from './Components/Layout/Layout'
-import Navbar from './Components/navbar/Navbar'
-import Header from './Components/header/Header'
-import DateRangePicker from './Components/datePicker/datePicker'
 
 const App = () => {
   return (
-    <Layout >
-      <Header />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-   
-    </Layout>
+    <div>
+      
+    </div>
   )
 }
 
